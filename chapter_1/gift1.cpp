2ae6f275ccf01aca81f1a621662f830532f4451a
Prak <100105525+prak132@users.noreply.github.com>
2023-01-21 19:43:23 -0800

    new file:   chapter_1/gift1.cpp 	new file:   chapter_1/povcoldpp.cpp 	renamed:    problemo/chapter_1/ride_is_here.cpp -> chapter_1/ride_is_here.cpp 	renamed:    problemo/chapter_1/test.cpp -> chapter_1/test.cpp 	new file:   cses/weird_algorithm.cpp 	deleted:    problemo/aaaaaaaaa

---
+++ chapter_1/gift1.cpp
@@ -0,0 +1,17 @@
+/*
+ID: Prakhar Rathore [rathore6]
+TASK: gift1
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    return 0;
+}
