ba9619bbedc9ffa1ca3b3e8c84f955ef0cf6e1ca
Prak <100105525+prak132@users.noreply.github.com>
2023-04-05 17:01:56 -0700

    Changes to be committed: 	modified:   a.out 	new file:   codeforces/A/A1774.cpp 	new file:   codeforces/A/A231.cpp 	new file:   codeforces/A/A236.cpp 	new file:   codeforces/A/A4.cpp 	modified:   template.cpp
     Changes not staged for commit:
    	deleted:    .gitignore
    	modified:   README.md
    	deleted:    a.out
    	deleted:    alphastar/ex1.cpp
    	modified:   euler/2.cpp
    	deleted:    exte/icons/8ball.png
    	deleted:    exte/icons/cosmic.png
    	deleted:    exte/icons/cosmicAYO.png
    	deleted:    exte/manifest.json
    	deleted:    exte/popup/images/proof.png
    	deleted:    exte/popup/popup.html
    	modified:   template.cpp
    	deleted:    usaco/2022-2023/hungry_cow.cpp
    
     Untracked files:
    	.DS_Store
    	.vscode/
    	codeforces/A/A102951.cpp
    	codeforces/A/A104257.cpp
    	codeforces/A/A1791.cpp
    	codeforces/A/A1807.cpp
    	codeforces/A/A1812.cpp
    	codeforces/A/A281.cpp
    	codeforces/A/A282.cpp
    	codeforces/A/A318.cpp
    	codeforces/B/
    	codeforces/H/
    	rand/

--- codeforces/A/A231.cpp
+++ codeforces/A/A231.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 #define pb push_back
-#define endl "\n"
 #define int long long
+#define endl "\n"
 
 using namespace std;
-int a,b;
+int a,b,c,z;
 
 void solve() {
-    cin >> a >> b;
-    cout << a+b << endl;
+    cin >> a >> b >> c;
+    if (a >= 1 && b >= 1) {
+        z++;
+    }
+    else if (a >= 1 && c >= 1) {
+        z++;
+    }
+    else if (b >= 1 && c >= 1) {
+        z++;
+    }
 }
 signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int t; cin >> t;
     while(t--) {solve();}
+    cout << z << endl;
     return 0;
 }
---
+++ codeforces/A/A236.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define endl "\n"
+
+using namespace std;
+string str;
+int z;
+
+int main() {
+    cin >> str;
+    set<char>len;
+    for(int i = 0;i<str.size();i++){
+        len.insert(str[i]);
+    }
+    int a = len.size();
+    if (a%2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else {
+        cout << "IGNORE HIM!" << endl;
+    }
+}
