5758c8fa35f0cd687819be217c322a5f7458f38b
Prak <100105525+prak132@users.noreply.github.com>
2023-04-06 18:01:49 -0700

    funny number

--- codeforces/A/A69.cpp
+++ codeforces/A/A69.cpp
@@ -7,11 +7,11 @@ signed main() {
     int t,a=0,b=0,c=0,s1=0,s2=0,s3=0; cin >> t;
     for (int i=0; i<t; i++) {
         cin >> a >> b >> c;
-        s1+=a;s2+=b;s3+=c;
+        s1+=a;s2+=b;s3+=c;//idk could use array for faster exc meh its 800 problem
     } if (s1==0 && s2==0 && s3==0) {
         cout << "YES\n";
     } else {
         cout << "NO\n";
     }
     return 0;
-}
+}//funny number BRUH 30 MS
