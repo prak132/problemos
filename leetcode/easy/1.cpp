3cd86f7910b12800f74afd5bdc6cc87ec42f9fb0
Prak <100105525+prak132@users.noreply.github.com>
2023-04-09 15:33:42 -0700

    binary search

--- leetcode/easy/1.cpp
+++ leetcode/easy/1.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> temp;//was going to impl hashmap
-        for(int i=0;i<nums.size();i++){// binary search is atleast O(n^2)
-            temp.push_back({nums[i],i});
-        }
-        sort(temp.begin(),temp.end());
-        int x=0,y=nums.size()-1;
-        while(x<y){
-            int sum=temp[x].first+temp[y].first;
-            if(sum==target)
-                return {temp[x].second,temp[y].second};
-            else if(sum<target)
-                x++;
-            else{
-                y--;
+        for(int i=0; i<nums.size(); i++){//binary search method
+            for(int j=i+1; j<nums.size(); j++){//could've done hash map but lazy/more code
+                if(nums[i]+nums[j] == target)return{i,j};
             }
         }
-        return {-1,-1};
+        return {};
     }
 };
