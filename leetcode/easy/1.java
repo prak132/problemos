4fe19e5cda55f625f8b4121b192777f24378b253
Prak <100105525+prak132@users.noreply.github.com>
2023-04-09 15:38:02 -0700

    Geeksforgeeks :orz:

--- leetcode/easy/1.java
+++ leetcode/easy/1.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         for (int i = 0; i < nums.length; i++) {//simple algo approach
+         for (int i = 0; i < nums.length; i++) {//simple binary search approach
             int diff = target - nums[i];
             for (int j = i+1; j < nums.length; j++) {
                 if (diff == nums[j]) {
---
+++ leetcode/easy/412.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	vector<string> fizzBuzz(int n) {
+		vector<string>ans;
+		for(int i=1; i<n+1; i++) {
+			if(i%3==0 && i%5==0) {
+                ans.push_back("FizzBuzz");
+            }
+			else if(i%3==0) {
+                ans.push_back("Fizz");
+            }
+			else if(i%5==0) {
+                ans.push_back("Buzz");
+            }
+			else {//this was dum idk how to do this until GEEKSFORGEEKS
+				string x="";
+				int z=i;
+				while(z){
+					x+=to_string(z%10);
+					z/=10;
+				}
+				reverse(x.begin(), x.end());
+				ans.push_back(x);
+			}
+		}
+		return ans;
+	}
+};
