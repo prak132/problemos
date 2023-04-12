28c21b9e57d7e8a9885a2679a85f606e749c211d
Prak <100105525+prak132@users.noreply.github.com>
2023-04-12 16:59:24 -0700

    impl idk why it took so long :sob:

---
+++ codeforces/A/A110.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long //side eye at the bounds
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;cin >> n;
+    int c = 0;
+    while (n != 0) {
+        if (n % 10 == 4 || n % 10 == 7) {
+            c += 1;
+        } n /= 10;
+    } if (c == 4 || c == 7) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
