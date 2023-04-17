fb4f6e526456dc7b96278317ebb56a85f7f2e198
Prak <100105525+prak132@users.noreply.github.com>
2023-04-17 11:49:47 -0700

    SENOR DEAN

--- USACO/Bronze/milk_pails.cpp
+++ USACO/Bronze/milk_pails.cpp
@@ -11,14 +11,14 @@ int main() {
 	int close = 0;
 	for (int i = 0; i <= M; i++) {
 		if (X * i > M) { 
-            break; 
+            break; //check how many we can fill in bucket 1 and check if too much
         }
-		for (int j = 0; j <= M; j++) {
+		for (int j = 0; j <= M; j++) { // check bucket 2 now
 			int n = (X * i) + (Y * j);
-			if (n > M) {
+			if (n > M) { //if ever greater 
                 break; 
             }
-			close = max(close, n);
+			close = max(close, n);//check so when we iterate will check
 		}
 	}
     cout << close << "\n";
