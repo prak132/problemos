f3090f6936423f00a5bd661689db55b3dccdbf2f
Prak <100105525+prak132@users.noreply.github.com>
2023-04-17 17:08:29 -0700

    yea

--- template.cpp
+++ template.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define int long long
-
 using namespace std;
-int a,b;
 
-void solve() {
-    cin >> a >> b;
-    cout << a+b << endl;
-}
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t; cin >> t;
-    while(t--) {solve();}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    int N; cin >> N;
     return 0;
 }
+
 //-std=c++17
 //-std=c++11
