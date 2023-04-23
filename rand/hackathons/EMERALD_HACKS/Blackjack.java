20b1ec078e79ae27c4a80dc24a56f9bd21d53d42
Prak <100105525+prak132@users.noreply.github.com>
2023-04-23 16:33:59 -0700

    old hackathon

---
+++ rand/hackathons/EMERALD_HACKS/Blackjack.java
@@ -0,0 +1,66 @@
+package rand.hackathons.EMERALD_HACKS;
+import java.util.Scanner;
+import java.util.*;
+ 
+public class Blackjack {
+   public static void main(String[] args) {
+        String anotherCard, redo = "y", ctn = null;
+        //initalizing the variables to be used
+        int next;
+        int c1;
+        int c2;
+        int dX;
+        int dY;
+        int pTotal = 0;
+        int dTotal = 0;
+        Scanner n = new Scanner(System.in);
+        Random random = new Random();
+        while ("y".equals(redo)) {
+         dX = random.nextInt(10) + 1;
+         dY = random.nextInt(10) + 1;
+            c1 = random.nextInt(10) + 1;
+            c2 = random.nextInt(10) + 1;
+            pTotal = c1 + c2;
+            dTotal = dX + dY;
+            System.out.println( "Dealer has: " + "\033[1;31m" + dX + "\u001B[0m");
+            System.out.println("Cards: " + "\033[1;32m" + c1 + ", " + c2 + "\u001B[0m");
+            System.out.println("Total: " + "\033[1;36m" + pTotal + "\u001B[0m");
+            System.out.println("Another Card? (y or n): ");
+            anotherCard = n.nextLine();
+            while ("y".equals(anotherCard)) {
+                next = random.nextInt(10) + 1;
+                pTotal += next;
+                System.out.println("Card: " + "\033[1;32m" + next + "\u001B[0m");
+                System.out.println("Total: " + "\u001B[32m" + pTotal + "\u001B[0m");
+                if (pTotal > 21) {
+                    System.out.println("\033[1;31m" + "Bust dealer wins" + "\u001B[0m");
+                    System.out.println("wanna play again? y/n");
+                    redo = n.nextLine();
+                }
+                if (pTotal < 21)
+                    System.out.println("Another Card (y/n)?: ");
+                anotherCard = n.nextLine();
+                if ("n".equals(anotherCard))
+                    System.out.println("Dealer had: " + dTotal);
+                System.out.println("You had: " + "\033[1;32m"+ pTotal + "\u001B[0m");
+                while ("n".equals(anotherCard)) {
+                    if (dTotal < pTotal && pTotal < 21) {
+                        System.out.println("\033[1;33m" + "Winner winner chicken dinner" + "\u001B[0m");
+                        System.out.println("Play again? (y/n): ");
+                        redo = n.nextLine();
+                        if (redo.equalsIgnoreCase("y"))
+                            redo = "y";
+                        if (dTotal > pTotal && dTotal < 21)
+                            System.out.println("\u001B[32m" + "Went over, lost" + "\u001B[0m");
+                        if (dTotal == pTotal)
+                           System.out.println("\u001B[35m" + "Tied so house wins lmao" + "\u001B[0m");
+                           redo = n.nextLine();
+                  }
+                }
+            }
+        }
+    }
+    
+}
+
+
---
+++ rand/hackathons/EMERALD_HACKS/main.java
@@ -0,0 +1,122 @@
+package rand.hackathons.EMERALD_HACKS;
+import java.util.Scanner;
+class Main {
+    public static boolean[][] shipArray = new boolean[10][10];
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        Scanner n = new Scanner(System.in);
+        System.out.println(
+                "Welcome to Battleship! Your objective is to sink all of the enemy's ships."
+                        + "There's one 2-spaced ship and one 3-spaced ship hidden below the surface.");
+        System.out.println();
+        System.out.print("Please enter the length of the board (Between 3-9): ");
+            int a = n.nextInt();
+            if (a < 3 || a > 9){
+                System.out.println("Board value " + "\u001B[3m" + "exceed limit" + "\u001B[0m" + " please try again");
+                System.out.print("Choose " + "\u001B[32m" + "Board value" +  "\u001B[0m" + " of grid: ");
+                a = n.nextInt();
+            }
+            else{
+            System.out.println("New grid layout is " + a + "X" + a);
+            wait(1000);
+            }
+        boolean[][] gridLayout = new boolean[a+1][a+1];
+        layout(gridLayout);
+        int numberOfTry = 0;
+        int numberOfHits = 0;
+        int numberOfSquares = 5;
+        boolean[][] shipArray = new boolean[a+1][a+1];
+        int randStartRow = (int) (Math.random() * (a));
+        int randStartCol = (int) (Math.random() * (a));
+        int storeShip1Row = (int) (Math.random() * (a));
+        int storeShip1Col = (int) (Math.random() * (a));
+        shipArray[storeShip1Row][randStartCol] = true;
+        shipArray[randStartRow][storeShip1Col] = true;
+        if (storeShip1Row == a) {
+            storeShip1Row--;
+            shipArray[storeShip1Row][randStartCol] = true;
+        } else {
+            storeShip1Row++;
+            shipArray[storeShip1Row][randStartCol] = true;
+        }
+        if (storeShip1Col + 3 <= a) {
+            shipArray[randStartRow][storeShip1Col + 1] = true;
+            shipArray[randStartRow][storeShip1Col + 2] = true;
+        } else {
+            shipArray[randStartRow][storeShip1Col - 1] = true;
+            shipArray[randStartRow][storeShip1Col - 2] = true;
+        }
+        while (numberOfHits != numberOfSquares) {
+            System.out.println();
+            System.out.print("Pick a row: ");
+            int rowInput = n.nextInt();
+            System.out.print("Pick a column: ");
+            int columnInput = n.nextInt();
+            if (rowInput > a || rowInput < 0 || columnInput > a || columnInput < 0) {
+                System.out.println("Input not allowed in grid. Please try again");
+                System.out.print("Pick a row: ");
+                rowInput = n.nextInt();
+                System.out.print("Pick a column: ");
+                columnInput = n.nextInt();
+            }
+            if (shipArray[rowInput][columnInput] && !gridLayout[rowInput][columnInput]) {
+                System.out.println("It's a hit! ");
+                numberOfHits += 1;
+                gridLayout[rowInput][columnInput] = true;
+                layout(gridLayout);
+            } else if (gridLayout[rowInput][columnInput]) {
+                System.out.println("You have already got a hit in this spot");
+                layout(gridLayout);
+            }
+            else {
+                gridLayout[rowInput][columnInput] = true;
+                System.out.println("It's a miss!");
+                layout(gridLayout);
+                numberOfTry++;
+            }
+            if (numberOfHits == numberOfSquares) {
+                int hitAccuracy = 100 * numberOfHits / (numberOfTry + numberOfHits);
+                int totalGuesses = numberOfTry + numberOfHits;
+                // int so auto rounds
+                System.out.println("Congratulations!");
+                System.out.println("Your hit percentage was: " + hitAccuracy + "%!");
+                System.out.println("The total guesses you took were: " + totalGuesses + "!");
+                long duration = (System.nanoTime() - start) / 1000000000;
+                long finalDuration = duration/= 60;
+                System.out.println("Your overall time was: " + finalDuration + " minutes!");
+
+                break;
+            }
+        }
+    }
+    public static void wait(int ms)
+{
+    try
+    {
+        Thread.sleep(ms);
+    }
+    catch(InterruptedException ex)
+    {
+        Thread.currentThread().interrupt();
+    }
+}
+    public static void layout(boolean[][] gridLayout) {
+        int storeNumOut = 0; 
+        System.out.print("  0  1  2  3  4  5  6  7  8  9");
+        for (int r = 0; r < gridLayout.length; r++) {
+            System.out.println();
+            System.out.print(storeNumOut);
+            storeNumOut += 1;
+            for (int c = 0; c < gridLayout[r].length; c++) {
+                if (gridLayout[r][c] == false) {
+                    System.out.print(" - ");
+                } else if (gridLayout[r][c] && shipArray[r][c]) {
+                    System.out.print("\u001B[32m" + " X " + "\u001B[0m");
+                } else {
+                    System.out.print("\u001B[31m" + " O " + "\u001B[0m");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
