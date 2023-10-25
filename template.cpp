2e590be7280bf94fdc06f7635d9643c1e44a8292
prak132 <prakharissuper@gmail.com>
2023-10-24 20:01:28 -0700

    using a new ide lets see how this goes
    oops

--- template.cpp
+++ template.cpp
@@ -1,21 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void setIO(string name = "") {
-	if (name.size()) {
-		freopen((name + ".in").c_str(), "r", stdin);
-		freopen((name + ".out").c_str(), "w", stdout);
-	}
-}
-
-
 void solve() {
     
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);cin.tie(nullptr);
-    setIO("io");
     int t; cin >> t;
+    while (t--) {solve();}
+    cout << "working\n";
     return 0;
 }
