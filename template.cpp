d0febb909439e47642b20c76574a6ef4eb967c37
prak132 <prakharissuper@gmail.com>
2023-12-24 22:46:02 -0800

    idk

--- template.cpp
+++ template.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 typedef pair<int, int> pii;
 #define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
+#define f first
+#define s second
 #define endl '\n'
 #define io(file) freopen(file".in", "r", stdin); freopen(file".out", "w", stdout);
-const int MOD = 1e9 + 7;
 
 void solve() {
     
@@ -17,7 +16,8 @@ void solve() {
 
 int main() {
 	ios_base::sync_with_stdio(false);cin.tie(nullptr);
-    int t; cin >> t;
+    int t=1; 
+    // cin >> t;
     while (t--) {solve();}
     return 0;
 }
